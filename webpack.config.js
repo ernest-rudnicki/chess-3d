6f16af5f79fa31e33a059a74515ffb74c90f18ee
Ernest Rudnicki <rudnicki.ernest@gmail.com>
2022-08-05 22:11:30 +0200

    feat: minor config adjustements

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,8 @@ module.exports = {
     },
     resolve: {
         alias: {
-          three: path.resolve('./node_modules/three')
+          three: path.resolve('./node_modules/three'),
+          scenes: path.resolve(__dirname, './src/scenes') 
         },
         extensions: ['.ts', '.js']
     },
